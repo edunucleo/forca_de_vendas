83614bbf5909f0bf8c429d776c6c06aaa9a84a72
edunucleo <edunucleo@gmail.com>
2022-09-17 17:56:51 -0300

    melhorias na lista2.usando imagens locais

--- lib/screens/pedido.dart
+++ lib/screens/pedido.dart
@@ -1,68 +1,84 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:forca_de_vendas/models/listaprodutos.dart';
 import 'package:forca_de_vendas/models/produto.dart';
 
 class Pedido extends StatefulWidget {
-
   @override
   _PedidoState createState() => _PedidoState();
-
 }
 
 class _PedidoState extends State<Pedido> {
   List<Produto> produtos = ListaProdutos.carregaProdutos();
   bool isSearching = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: !isSearching?
-        const Text('Pedido'):
-        TextField(
-          autofocus: true,
-          style: TextStyle(color: Colors.white),
-            decoration: InputDecoration(
-              icon: Icon(Icons.search,color: Colors.white),
-              hintText: 'Buscar Item',
-              hintStyle: TextStyle(color:Colors.white),
-            ),
-        ),
+        title: !isSearching
+            ? const Text('Pedido')
+            : TextField(
+                autofocus: true,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.search, color: Colors.white),
+                  hintText: 'Buscar Item',
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+              ),
         actions: [
-          isSearching?
-          IconButton(
-            onPressed: () {
-              setState(() {
-                this.isSearching=!this.isSearching;
-              });
-            },
-            icon: Icon(Icons.cancel),
-          )
-          :
-          IconButton(
-            onPressed: () {
-              setState(() {
-                this.isSearching=!this.isSearching;
-              });
-            },
-            icon: Icon(Icons.search),
-          ),
+          isSearching
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      this.isSearching = !this.isSearching;
+                    });
+                  },
+                  icon: Icon(Icons.cancel),
+                )
+              : IconButton(
+                  onPressed: () {
+                    setState(() {
+                      this.isSearching = !this.isSearching;
+                    });
+                  },
+                  icon: Icon(Icons.search),
+                ),
         ],
       ),
-
       body: ListView.builder(
         itemCount: produtos.length,
-        prototypeItem: ListTile(
-          title: Text(produtos.toString()),
-        ),
+        // prototypeItem: ListTile(
+        //    title: Text(produtos.toString()),
+        //  ),
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: Image.network('https://media-exp1.licdn.com/dms/image/C4D03AQHSKexgrFF--Q/profile-displayphoto-shrink_100_100/0/1516956407194?e=1668643200&v=beta&t=S7fI8bSsbkamuxytC-pICWKqFtJwHULDwUmKvSiYZn0'),
-            title: Text(produtos[index].nome),
-            trailing: Text(produtos[index].preco.toString()),
-            onTap: (){
-              debugPrint('clicou');
-            },
-
+          return Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Color(0xcc000000),
+                      offset: Offset(0, 2),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+                height: 80,
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    Image.asset('assets/images/foto.jpg'),
+                    Text(produtos[index].nome),
+                    Text(produtos[index].preco.toString())
+                  ],
+                ),
+              ),
+            ],
           );
         },
       ),
