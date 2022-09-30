85a7e0256ffcd8a6a728b1d921c6b62950d0d1f8
edunucleo <edunucleo@gmail.com>
2022-09-30 12:15:42 -0300

    melhoria na tela produto-screen.dart

--- lib/screens/produto-screen.dart
+++ lib/screens/produto-screen.dart
@@ -1,8 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:forca_de_vendas/models/produto.dart';
-class ProdutoScreen extends StatefulWidget{
+import 'package:intl/intl.dart';
+
+class ProdutoScreen extends StatefulWidget {
   Produto produto;
+
   ProdutoScreen(this.produto);
+
   @override
   _ProdutoScreenState createState() => _ProdutoScreenState();
 }
@@ -10,11 +16,99 @@ class ProdutoScreen extends StatefulWidget{
 class _ProdutoScreenState extends State<ProdutoScreen> {
   @override
   Widget build(BuildContext context) {
+    var f = NumberFormat("###.0#", "pt_BR");
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.produto.nome),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.produto.nome),
+        ),
+        body: ListView(
+
+          children: [
+            // Column(
+            //
+            //  children: [
+                Container(
+                  margin: const EdgeInsets.all(30),
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          color: Color(0xcc000000),
+                          offset: Offset(0, 2),
+                          blurRadius: 4,
+                        ),
+                      ],
+                      border: Border.all(
+                        color: Colors.black,
+                        //width: 10.0,
+                      ),
+                      image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/${widget.produto.imagem}'),
+                        fit: BoxFit.cover,
+                      )),
+                ),
+                Text.rich(
+                  TextSpan(
+                    style: TextStyle(fontSize: 20),
+                    text: 'R\$', // default text style
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: f.format(widget.produto.preco),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Card(
+                  color: Colors.lightBlueAccent,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Ink(
+                          decoration: ShapeDecoration(
+                            color: Colors.blueAccent,
+                            shape: CircleBorder(),
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.remove),
+                            onPressed: () {
+                              print('pressed');
+                            },
+                          )),
+                      Container(
+                        width: 50,
+                        margin: const EdgeInsets.all(15),
+                        child: TextFormField(
+                            initialValue: '1',
+
+                            textAlign: TextAlign.center
+                        ),
+                      ),
+                      Ink(
+                          decoration: ShapeDecoration(
+                            color: Colors.blueAccent,
+                            shape: CircleBorder(),
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () {
+                              print('pressed');
+                            },
+                          )),
+                    ],
+                  ) ,
+                ),
+
+
+
+            //   ],
+            // ),
+          ],
+        ));
   }
 }
-
